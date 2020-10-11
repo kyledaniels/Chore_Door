38fdc4def1b10169e61b4850bd6ca250b1602704
kyle67 <daniels.kyle@gmail.com>
2020-10-11 13:50:55 -0400

    added onclick event to door to change from closed door to robot

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-let doorImage1 = documnent.getElementById('door1');
+let doorImage1 = document.getElementById('door1');
+let botDoorPath = 'https://content.codecademy.com/projects/chore-door/images/robot.svg'
 
 doorImage1.onclick = ()=>{
-
+   doorImage1.src = botDoorPath;
 }
