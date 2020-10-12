1d32538f49982736603286c5ab5dddd36195712e
kyle67 <daniels.kyle@gmail.com>
2020-10-11 20:37:33 -0400

    created function to move robot to different doors

--- script.js
+++ script.js
@@ -4,6 +4,21 @@ let doorImage3 = document.getElementById('door3');
 let botDoorPath = 'https://content.codecademy.com/projects/chore-door/images/robot.svg';
 let beachDoorPath = 'https://content.codecademy.com/projects/chore-door/images/beach.svg';
 let spaceDoorPath = 'https://content.codecademy.com/projects/chore-door/images/space.svg';
+let numClosedDoors = 3;
+let openDoor1;
+let openDoor2;
+let openDoor3;
+
+const randomChoreDoorGenerator = ()=>{
+    let choreDoor = Math.floor(Math.random()*numClosedDoors);
+      if(choreDoor===0){
+
+      }else if(choreDoor===1){
+          
+      }else if (choreDoor===2){
+
+      }
+}
 
 doorImage1.onclick = ()=>{
    doorImage1.src = botDoorPath;
